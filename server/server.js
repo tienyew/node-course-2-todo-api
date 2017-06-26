ab97ca66243616cfe37bc4f5df79030888a5ed96
Tien Yew <tienyew@gmail.com>
2017-06-27 09:51:46 +1000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,18 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send('ID not valid');
+  }
+
+  Todo.findById(id)
+    .then((todo) => !todo ? res.status(404).send('Todo not found') : res.send({todo})
+    )
+    .catch((e) => res.status(400).send(e));
 });
 
 app.listen(3000, () => {
