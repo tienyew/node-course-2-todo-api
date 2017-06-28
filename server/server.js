3f7bf81642be8b7787e1f0e84f2f575dc4b9ee2d
Tien Yew <tienyew@gmail.com>
2017-06-28 23:45:53 +1000

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -99,6 +99,7 @@ app.patch('/todos/:id', (req,res) => {
 })
 
 app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
   user.save().then(() => {
